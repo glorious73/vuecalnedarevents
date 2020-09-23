ad890a9181f76a976ec53352f969d87143edd20e
glorious73 <amjadmh73@gmail.com>
2020-09-23 13:39:53 +0300

    Added SeedData. getActiveDay getter. SET_ACTIVE_DAY mutation and its action

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,32 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { seedData } from '../seed.js';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
+const store = new Vuex.Store({
   state: {
+    seedData: seedData
+  },
+  getters: {
+    getActiveDay: (state) => {
+        return state.seedData.find((day) => day.active);
+    }
   },
   mutations: {
+    SET_ACTIVE_DAY (state, dayId) {
+        state.seedData.map((dayObj) => { 
+          (dayObj.id === dayId) ? dayObj.active = true : dayObj.active = false; 
+        });
+    }
   },
   actions: {
+    setActiveDay(context, dayId) {
+      context.commit('SET_ACTIVE_DAY', dayId);
+    }
   },
   modules: {
   }
-})
+});
+
+export default store;
